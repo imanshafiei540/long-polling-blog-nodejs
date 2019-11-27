de0e3d9c20ddc0e294b777224ba2fd6d11c2f50e
imanshafiei <imanshafiei@ymail.com>
2019-11-27 11:51:55 +0330

    Initial Commit

---
+++ server.js
@@ -0,0 +1,116 @@
+const path = require('path');
+
+var port = process.env.PORT || 8000;
+var MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+var url = "mongodb://localhost:27017/";
+
+var waitingRequests = [];
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.listen(port);
+
+function respond(response, status, data, type) {
+    response.writeHead(status, {
+        "Content-Type": type || "text/plain"
+    });
+    response.end(data);
+}
+
+function respondJSON(response, status, data) {
+    respond(response, status, JSON.stringify(data),
+        "application/json");
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/admin', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/admin.html'));
+});
+
+app.post('/post', function (req, res) {
+    var post_title = req.body.post.title;
+    var post_description = req.body.post.description;
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myobj = {title: post_title, description: post_description, likes: 0};
+        dbo.collection("posts").insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+    sendChanges();
+    respondJSON(res, 200);
+});
+
+app.get('/post', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("posts").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            respondJSON(res, 200, result);
+            db.close();
+        });
+    });
+});
+
+app.put('/post', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myquery = {_id: ObjectId(req.body.post.post_id)};
+        var newvalues = {$set: {title: req.body.post.title, description: req.body.post.description}};
+        dbo.collection("posts").updateOne(myquery, newvalues, function (err, res) {
+            console.log(res);
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
+        respondJSON(res, 200);
+    });
+});
+
+app.get('/changes', function (req, res) {
+    waitForChanges(res);
+});
+
+function waitForChanges(response) {
+    var waiter = {response: response};
+    waitingRequests.push(waiter);
+    setTimeout(function () {
+        var found = waitingRequests.indexOf(waiter);
+        if (found > -1) {
+            waitingRequests.splice(found, 1);
+            var data = {};
+            data.posts = [];
+            respondJSON(response, 200, data);
+        }
+    }, 30 * 1000);
+}
+
+function sendChanges() {
+    while (waitingRequests.length > 0) {
+        var request = waitingRequests.shift();
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.collection("posts").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                respondJSON(request.response, 200, result);
+                db.close();
+            });
+        });
+
+    }
+}
